2a31aa57c51ad1f819d4e1c98e32f15a8869a43f
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-12-13 15:47:41 -0500

    update rspec test seed

--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -3,24 +3,45 @@ require 'rails_helper'
 RSpec.describe Reading, type: :model do
   before :all do
 
+	# user_1 = User.create(
+	#   email: 'dev@meter-man.com',
+	#   password: '12345678',
+	#   password_confirmation: '12345678',
+	#   username: 'Spiderman'
+	# )
+
+	# # Create 10 reading for the one and only user
+	# (1..10).each do |i|
+	#   month = format("%02d", i)
+	#   Reading.create(
+	#     reading: 1000 * i,
+	#     reading_date: "2015-#{month}-01",
+	#     created_at: "2015-#{month}-01 [12:00:00]",
+	#     updated_at: "2015-#{month}-01 [12:00:00]",
+	#     user_id: user_1.id
+	#   )
+	# end
 	user_1 = User.create(
+	  username: 'dev',
+	  zip: 10573,
 	  email: 'dev@meter-man.com',
 	  password: '12345678',
-	  password_confirmation: '12345678',
-	  username: 'Spiderman'
+	  password_confirmation: '12345678'
 	)
 
-	# Create 10 reading for the one and only user
-	(1..10).each do |i|
-	  month = format("%02d", i)
-	  Reading.create(
-	    reading: 1000 * i,
-	    reading_date: "2015-#{month}-01",
-	    created_at: "2015-#{month}-01 [12:00:00]",
-	    updated_at: "2015-#{month}-01 [12:00:00]",
-	    user_id: user_1.id
-	  )
-	end
+# Create 10 reading for the one and only user
+		reading = 1000
+			(5..64).each do |i|
+			  month = format("%02d", i/5)
+			  day = format("%02d", (i%5+1)*3)
+			  Reading.create(
+			    reading: reading += 250 + Random.rand(200),
+			    reading_date: "2015-#{month}-#{day}",
+			    created_at: "2015-#{month}-#{day} [12:00:00]",
+			    updated_at: "2015-#{month}-#{day} [12:00:00]",
+			    user_id: user_1.id
+			  )
+		end
 
   end
 
@@ -55,4 +76,5 @@ RSpec.describe Reading, type: :model do
  	reading.valid?
  	expect(reading.errors[:reading]).to include('is not within allowable range')
  end
+
 end
