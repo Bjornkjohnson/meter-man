817176fdef2ed90607462766af51040efc587d76
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-12-14 14:05:50 -0500

    add console log

--- app/assets/javascripts/readings.js
+++ app/assets/javascripts/readings.js
@@ -56,6 +56,7 @@
           .attr("class", "x axis")
           .attr("transform", "translate(0," + height + ")")
           .call(xAxis);
+      console.log(here);
 
       svg.append("g")
           .attr("class", "y axis")
